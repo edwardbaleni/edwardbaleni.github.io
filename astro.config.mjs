83778da0a671bfc55b5609486d72d1875559ba7b
edwardbaleni <baleniedward@gmail.com>
2025-08-28 15:20:03 +0200

    Added actions, configured to work on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  // site: "https://astro-nano-demo.vercel.app",
+  site: "https://edwardbaleni.github.io/",
+  base: "/whats-what",
   integrations: [mdx(), sitemap(), tailwind()],
 });
