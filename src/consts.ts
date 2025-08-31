3f6c37cf8ecbe7280f40ffc9eb83a6796c4ff8b4
edwardbaleni <baleniedward@gmail.com>
2025-08-31 21:39:36 +0200

    Image height changed to a fraction

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Nano", // Site name
+  NAME: "Edward Baleni", // Site name
   EMAIL: "baleniedward@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
